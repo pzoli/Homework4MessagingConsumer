adbe3140ffd76c074da9b82536341978e2e3a239
pzoli <papp.zoltan@integrity.hu>
2025-04-25 12:47:27 +0200

    logger instead system.out

--- src/main/java/hu/infokristaly/messagingconsumer/jms/MyBean.java
+++ src/main/java/hu/infokristaly/messagingconsumer/jms/MyBean.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.logging.Logger;
 
 import hu.infokristaly.messagingconsumer.jms.dtos.Customer;
 
@@ -14,14 +15,17 @@ public class MyBean {
     
 	@Autowired
 	private ObjectMapper objectMapper;
+	
+	private static final Logger logger = Logger.getLogger("FileInfoController");
 
     @JmsListener(destination = "someQueue", containerFactory = "myFactory")
 	public void processMessage(Customer content) {
 		String jsonString;
 		try {
 			jsonString = objectMapper.writeValueAsString(content);
-			System.out.println("Received message: " + jsonString);
+			logger.info("Received message: " + jsonString);
 		} catch (JsonProcessingException e) {
+			logger.severe("Error processing message: " + e.getMessage());
 			e.printStackTrace();
 		}
 	}
