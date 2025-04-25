fa3ca0383f9ac7ea18bdd9fa3d44069456ec41fe
pzoli <papp.zoltan@integrity.hu>
2025-04-25 12:05:31 +0200

    customer to json

--- src/main/java/hu/infokristaly/messagingconsumer/jms/MyBean.java
+++ src/main/java/hu/infokristaly/messagingconsumer/jms/MyBean.java
@@ -1,15 +1,28 @@
 package hu.infokristaly.messagingconsumer.jms;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import hu.infokristaly.messagingconsumer.jms.dtos.Customer;
 
 @Component
 public class MyBean {
     
+	@Autowired
+	private ObjectMapper objectMapper;
+
     @JmsListener(destination = "someQueue", containerFactory = "myFactory")
 	public void processMessage(Customer content) {
-		System.out.println("Received message: " + content);
+		String jsonString;
+		try {
+			jsonString = objectMapper.writeValueAsString(content);
+			System.out.println("Received message: " + jsonString);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
 	}
 }
